493c0930ca96436d4ee0a251c77db64b42a608e4
ikiusen <thiagofcabrera00@gmail.com>
2020-09-13 11:30:30 -0300

    TP hecho sin errores

--- TP 5/listadoalumnos.cpp
+++ TP 5/listadoalumnos.cpp
@@ -5,35 +5,41 @@
 using namespace std;
 
 const int MAX = 50;
-struct  Alumno {
+typedef struct{
     string nombre;
     string apellido;
     string DNI;
     int edad;
-    double nota;
-};
-Alumno lista[MAX];
-int contador;
-int i;
-string tempText;
+    int nota;
+} tAlumno;
 
-void cargarArchivo();
-void guardarArchivo();
-void addAlumno();
-void eliminarAlumno(string nombre);
-void calificarAlumno();
-void mostrarAlumnos();
+typedef tAlumno tArray[MAX];
+
+typedef struct{
+    tArray elementos;
+    int contador;
+} tLista;
+
+int i;
+
+void cargarArchivo(tLista &lista);
+void guardarArchivo(tLista &lista);
+void addAlumno(tLista &lista);
+void eliminarAlumno(tLista &lista, string nombre, string apellido);
+void calificarAlumno(tLista &lista);
+void mostrarNotasAlumnos(tLista &lista);
 
 int main(){
-
+    tLista lista;
+    string tempText,tempText1;
     int op;
-    contador = 0;
-    cargarArchivo();
+    lista.contador = 0;
+    cargarArchivo(lista);
     do {
         cout << "1 - Añadir un nuevo alumno" << endl;
         cout << "2 - Eliminar un alumno" << endl;
         cout << "3 - Calificar alumnos" << endl;
-        cout << "4 - Listado de estudiantes" << endl;
+        cout << "4 - Listado de notas" << endl;
         cout << "0 - Salir" << endl;
         cin >> op;
 
@@ -41,22 +47,27 @@ int main(){
         {
         case 1:
             //Añadir alumnos
-            addAlumno();
+            addAlumno(lista);
             break;
         case 2:
-            cout << "Ingrese el nombre del alumno que desee eliminar" << endl;
+            //Eliminar alumno
+            cout << "Ingrese el nombre del alumno que desee eliminar: ";
             cin >> tempText;
-            eliminarAlumno(tempText);
+            cout << "Ingrese el apellido del alumno que desee eliminar: ";
+            cin >> tempText1;
+            eliminarAlumno(lista,tempText, tempText1);
             break;
         case 3:
             //Calificar alumnos
+            calificarAlumno(lista);
+            break;
         case 4:
-            //Listado alumnos
-
+            //Listado de notas alumnos
+            mostrarNotasAlumnos(lista);
             break;
         case 0:
             //Guardamos y salimos
-            guardarArchivo();
+            guardarArchivo(lista);
             break;
         default:
             cout << "Opcion desconocida!" << endl;
@@ -68,7 +79,7 @@ return 0;
 
 
 
-void cargarArchivo()
+void cargarArchivo(tLista &lista)
 {
     ifstream cArchivo;
     cArchivo.open("AyED.txt", ios::in);
@@ -76,68 +87,117 @@ void cargarArchivo()
     {
         while (!cArchivo.eof())
         {
-            cArchivo >> lista[contador].nombre;
+            cArchivo >> lista.elementos[lista.contador].nombre;
             cArchivo.get( );
-            cArchivo >> lista[contador].apellido;
+            cArchivo >> lista.elementos[lista.contador].apellido;
             cArchivo.get( );
-            cArchivo >> lista[contador].DNI;
-            cArchivo >> lista[contador].edad;
-            cArchivo >> lista[contador].nota;
+            cArchivo >> lista.elementos[lista.contador].DNI;
+            cArchivo >> lista.elementos[lista.contador].edad;
+            cArchivo >> lista.elementos[lista.contador].nota;
 
-            if((!lista[contador].nombre.empty())&&(!lista[contador].apellido.empty())&&(!lista[contador].DNI.empty())&&(lista[contador].edad != 0)&&(lista[contador].nota >= 0))
+            if((!lista.elementos[lista.contador].nombre.empty())&&(!lista.elementos[lista.contador].apellido.empty())&&(!lista.elementos[lista.contador].DNI.empty())&&(lista.elementos[lista.contador].edad != 0)&&(lista.elementos[lista.contador].nota >= 0))
             {
-                contador++;
+                lista.contador++;
             }
         }
     }
     cArchivo.close();
 }
-void guardarArchivo()
+
+void guardarArchivo(tLista &lista)
 {
     ofstream gArchivo;
     gArchivo.open("AyED.txt", ios::out);
     if(gArchivo.is_open())
     {
-        for(i = 0; i < contador; i++)
+        for(i = 0; i < lista.contador; i++)
         {
-             gArchivo << lista[i].nombre << " " << lista[i].apellido << " " <<lista[i].DNI << " " << lista[i].edad << " " << lista[i].nota <<endl;
+             gArchivo << lista.elementos[i].nombre<< " " << lista.elementos[i].apellido << " " <<lista.elementos[i].DNI << " " << lista.elementos[i].edad << " " << lista.elementos[i].nota <<endl;
         }
     }
     gArchivo.close();
 }
-void addAlumno()
+
+void addAlumno(tLista &lista)
 {
-    if(contador < MAX)
+    if(lista.contador < MAX)
     {
         cout << "Ingrese el nombre del alumno: ";
         cin.ignore();
-        getline(cin,lista[contador].nombre);
+        getline(cin,lista.elementos[lista.contador].nombre);
         cout << "Ingrese el apellido del alumno: ";
-        getline(cin,lista[contador].apellido);
+        getline(cin,lista.elementos[lista.contador].apellido);
         cout << "Ingrese el DNI del alumno: ";
-        getline(cin,lista[contador].DNI);
+        getline(cin,lista.elementos[lista.contador].DNI);
         cout << "Ingrese la edad del alumno: ";
-        cin >> lista[contador].edad;
-        contador = (contador + 1);
+        cin >> lista.elementos[lista.contador].edad;
+        lista.elementos[lista.contador].nota = 0;
+        lista.contador = (lista.contador + 1);
 
     } else
         cout << "Maximo de archivos alcanzados (50)!" << endl;
 }
 
-void eliminarAlumno(string nombre)
+void eliminarAlumno(tLista &lista, string nombre, string apellido)
 {
-    for(i=0; i < contador; i++)
+    int pos = 0;
+    for(i = 0; i < lista.contador; i++)
     {
-        if(lista[i].nombre == nombre)
+        if((lista.elementos[i].nombre == nombre)&&(lista.elementos[i].apellido == apellido))
         {
-            lista[i].nombre.clear();
-            lista[i].apellido.clear();
-            lista[i].DNI.clear();
-            lista[i].edad = 0;
-            lista[i].nota = 0;
-            contador = (contador - 1);
-            cout << "Alumno eliminado correctamente" << endl;
+            pos = i;
+            for(i = pos; i < lista.contador - 1; i++)
+            {
+             lista.elementos[i] = lista.elementos[i+1];
+            }
+            lista.contador = (lista.contador - 1);
+            cout << "Alumno eliminado correctamente!" << endl;
         }
     }
 
+}
+
+void calificarAlumno(tLista &lista)
+{
+    int nota;
+    for(i=0; i < lista.contador; i++)
+    {
+        cout << "Nombre del alumno: " << lista.elementos[i].nombre << endl;
+        cout << "Apellido del alumno: " << lista.elementos[i].apellido << endl;
+        cout << "Ingrese nota del alumno: ";
+        cin >> nota;
+        if((nota>=1)&&(nota <=10))
+        {
+            lista.elementos[i].nota = nota;
+        } else {
+            cout << "Nota no valida!" << endl;
+            cout << "Reingrese la nota de: " << lista.elementos[i].nombre << " " << lista.elementos[i].apellido << endl;
+            cin >> nota;
+            lista.elementos[i].nota = nota;
+        }
+
+    }
+}
+void mostrarNotasAlumnos(tLista &lista)
+{
+    double media = 0;
+    int total, maxima = 0;
+    string nombre, apellido;
+
+    cout << "Listado de notas:" << endl;
+    for(i = 0; i < lista.contador; i++)
+    {
+        cout << lista.elementos[i].nota << endl;
+        if(lista.elementos[i].nota >= maxima)
+        {
+            maxima = lista.elementos[i].nota;
+            nombre = lista.elementos[i].nombre;
+            apellido = lista.elementos[i].apellido;
+        }
+        media = media + lista.elementos[i].nota;
+        total = i;
+    }
+    media = (media/i);
+    cout << "La nota mas alta pertenece a: " << nombre << " " << apellido << " y es: " << maxima << endl;
+    cout << "La nota media es: " << media << endl;
 }
